a10989fcaa2e097bb73d58f943921450210a97e6
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-06-22 18:55:03 +0200

    complete project

--- js/script.js
+++ js/script.js
@@ -14,6 +14,10 @@ const ageInput = document.getElementById('age-selection');
 // * Bersaglio la section your-tickets
 const yourTicketsSection = document.getElementById('your-tickets');
 
+// ! VALIDAZIONE DEGLI INPUT
+
+
+
 // * Qui invece vengono bersagliati tutti i paragrafi che verranno riempiti
 // * al click del bottone "Genera" che serviranno a compilare il biglietto
 const passengerInfoForTicket = document.querySelector('#passenger-info p');
@@ -29,50 +33,71 @@ const costsForTicket = document.querySelector('#ticket-costs p');
 const generateButton = document.getElementById('generate-ticket');
 
 generateButton.addEventListener('click',function(){
+    
+    
+    if(!(firstLastNameInput.value === '') && !(kmsNumberInput.value <= 0 || kmsNumberInput.value == null)){
 
-    /*
-    *** STEP 3 *** - Compiliamo il biglietto**** - Queste sono le informazioni standard senza alcuno sconto
-    */
+       /*
+       *** STEP 3 *** - Compiliamo il biglietto**** - Queste sono le informazioni standard senza alcuno sconto
+       */
 
-    passengerInfoForTicket.innerText = firstLastNameInput.value;
-    offerForTicket.innerText = 'Biglietto Standard';
-
-    // Calcoliamo in maniera randomica la carrozza e il Codice CP
-
-    // *** Carrozza ***
-
-    positionForTicket.innerText  = Math.floor(Math.random() * 20 + 1);
-
-    // *** Codice CP ***
-
-    codForTicket.innerText  = Math.floor(Math.random() * (29000 + 1 - 25000)) + 25000;
-
-    // Qui ci prendiamo l'indice della option scelta
-    const ageInputIndex = ageInput.selectedIndex;
-
-    // Qui invece prendiamo il valore corrispondente a quell'indice
-    const ageInputValue = ageInput.options[ageInputIndex];
-
-    // Logica che calcola il prezzo del biglietto
-
-    let ticketPrice = 0.21 * kmsNumberInput.value;
-
-    if(ageInputValue === 'U18'){
-        const underAgeSale = ticketPrice * 20 / 100;
-        ticketPrice = ticketPrice - underAgeSale;
-    }else if(ageInputValue === 'O65'){
-        const OverAgeSale = ticketPrice * 40 / 100;
-        ticketPrice = ticketPrice - OverAgeSale;
+   passengerInfoForTicket.innerText = firstLastNameInput.value;
+   
+   // Calcoliamo in maniera randomica la carrozza e il Codice CP
+   
+   // *** Carrozza ***
+   
+   positionForTicket.innerText  = Math.floor(Math.random() * 20 + 1);
+   
+   // *** Codice CP ***
+   
+   codForTicket.innerText  = Math.floor(Math.random() * (29000 + 1 - 25000)) + 25000;
+   
+   // Qui ci prendiamo l'indice della option scelta
+   const ageInputIndex = ageInput.selectedIndex;
+   
+   // Qui invece prendiamo il valore corrispondente a quell'indice
+   const ageInputValue = ageInput.options[ageInputIndex].value;
+   
+   // Logica che calcola il prezzo del biglietto
+   
+   let ticketPrice = 0.21 * kmsNumberInput.value;
+   
+      if(ageInputValue === 'U18'){
+          const underAgeSale = ticketPrice * 20 / 100;
+          ticketPrice = ticketPrice - underAgeSale;
+          offerForTicket.innerText = 'Biglietto scontato del 20%';
+        }else if(ageInputValue === 'O65'){
+            const OverAgeSale = ticketPrice * 40 / 100;
+            ticketPrice = ticketPrice - OverAgeSale;
+            offerForTicket.innerText = 'Biglietto scontato del 40%';
+        }else{
+            offerForTicket.innerText = 'Biglietto Standard';
+        }
+        
+        costsForTicket.innerHTML = ticketPrice.toFixed(2) + '<span> <i class="fa-solid fa-euro"></span>';
+        
+        /*
+        *** STEP 4 *** - Rendiamo visibile il biglietto appena compilato
+        */
+       
+       yourTicketsSection.classList.remove('d-none');
+       yourTicketsSection.classList.add('d-block');
+       
+    }else{
+        alert('Input Errati: fornire un nome e un numero di chilometri maggiore di 0');
     }
-
-    costsForTicket.innerText = ticketPrice;
-
+       
+    })
+    
     /*
-    *** STEP 4 *** - Rendiamo visibile il biglietto appena compilato
+    *** STEP 5 *** - Bersagliamo il bottone "Annulla" in modo tale da annulla i campi del form compilato
     */
-
-    yourTicketsSection.classList.remove('d-none');
-    yourTicketsSection.classList.add('d-block');
-
- 
-})
+   
+   const cancelButton = document.getElementById('cancel-ticket');
+   
+   cancelButton.addEventListener('click',function(){
+       firstLastNameInput.value = '';
+       kmsNumberInput.value = 0;
+       ageInput.value = 'U18';
+    })
